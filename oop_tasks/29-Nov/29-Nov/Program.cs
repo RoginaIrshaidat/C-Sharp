344f680fe2e2571696320e07cdce851aca9ef790
RoginaIrshaidat <115452721+RoginaIrshaidat@users.noreply.github.com>
2022-11-29 21:13:32 -0800

    update tasks

--- oop_tasks/29-Nov/29-Nov/Program.cs
+++ oop_tasks/29-Nov/29-Nov/Program.cs
@@ -9,40 +9,41 @@ namespace _29_Nov
     class Employee
     {
         public string employeeName { get; set; }
-        public int birth { get; set; }
+        public DateTime birth { get; set; }
         public int employeeId { get; set; }
 
-        public virtual void age(int year)
+        public  void age()
         {
-            int age = 2022 - year;
-            Console.WriteLine($"The employee age = {age}");
+            DateTime now = DateTime.Now; TimeSpan value = now.Subtract(birth);
+
+            Console.WriteLine((int)value.TotalDays / 365);
         }
 
-        public Employee(string emplName, int birth, int emplId)
+        
+
+        public Employee(string emplName, int day, int month, int year, int emplId)
         {
             this.employeeName = emplName;
-            this.birth = birth;
+            birth = new DateTime(year, month, day); ;
             this.employeeId = emplId;
         }
 
-        
-        
-        public void print()
+        public virtual void print()
         {
-            Console.WriteLine($"The Id of employee {employeeId},The Employee Name {employeeName} , the birth date  {birth} ");
+            Console.WriteLine($"The Employee Name {employeeName}");
         }
     }
     class Manager : Employee
     {
-        public Manager(string emplName, int birth, int emplId ): base ( emplName,  birth,  emplId)
+        public Manager(string emplName, int day, int month, int year, int emplId) : base(emplName, day, month, year, emplId)
         {
 
         }
-        public override void age(int year)
+        public override void print()
         {
-            int age = 2022 - year;
-            Console.WriteLine($"The manager age = {age}");
+            Console.WriteLine($"The Id of employee {employeeId},The Employee Name {employeeName} , the birth date  {birth} ");
         }
+
     }
     internal class Program
     {
@@ -51,19 +52,25 @@ namespace _29_Nov
             Console.WriteLine("Enter Employee name");
             string empName = Console.ReadLine();
 
-            Console.WriteLine("Enter birth date ");
-            int birth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter day of birthday date ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter month of birthday date ");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter day of year date ");
+            int year = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Enter the id");
             int ID = Convert.ToInt32(Console.ReadLine());
 
-            Employee emp = new Employee(empName, birth, ID);
-            emp.print();
-            emp.age(birth);
+            //Employee emp = new Employee(empName, day,month, year, ID);
+            //emp.print();
+            //emp.age(day,month,year);
 
-            Manager manager = new Manager("Rami",1996,50);
+            Manager manager = new Manager("Rami",11,11,1996,50);
             manager.print();
-            manager.age(1996);
+            manager.age();
         }
     }
 }
